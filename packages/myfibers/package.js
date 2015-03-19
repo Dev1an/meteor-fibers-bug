82b9978be952dcdbbc6263ad77e059bb7353cfc9
Devian <info@devian.tk>
2015-03-19 10:41:13 +0100

    delete unnecessary code and update meteor

--- packages/myfibers/package.js
+++ packages/myfibers/package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'myfibers',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
   summary: '',
-  // URL to the Git repository containing the source code for this package.
   git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
@@ -20,5 +16,4 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('myfibers');
-  api.addFiles('myfibers-tests.js');
 });
